f412a98099a1cffad127eb54c0add8c4e75926c0
Agustin Fascioli <agufascioli@gmail.com>
2024-05-21 23:52:38 -0300

    created an interface for the input output manager, added a cmd manager

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"go-price-calculator/filemanager"
+	"go-price-calculator/cmdmanager"
 	"go-price-calculator/prices"
 )
 
 func main() {
-	taxRates := []float64{0, 0.7, 0.1, 0.15}
+	taxRates := []float64{0.7, 0.3, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := *filemanager.New(
-			"prices.txt",
-			fmt.Sprintf("./results/results_%.0f.json", taxRate*100),
-		)
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		// fm := *filemanager.New(
+		// 	"prices.txt",
+		// 	fmt.Sprintf("./results/results_%.0f.json", taxRate*100),
+		// )
+		cmd := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(cmd, taxRate)
 		priceJob.Process()
 	}
 }
